48f57f1c6decb960942b6446fc11af43d0b149cb
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-03 11:17:25 +0100

    fix .env

--- config/filemanager.php
+++ config/filemanager.php
@@ -4,7 +4,7 @@ $CONFIG = '{"lang":"en","error_reporting":true,"show_hidden":true,"hide_Cols":fa
 $use_auth = true;
 
 $auth_users = [
-    getenv('FILEMANAGER_USERNAME') => password_hash(getenv('FILEMANAGER_PASSWORD'), PASSWORD_DEFAULT),
+    getenv('FILEMANAGER_USERNAME') => getenv('FILEMANAGER_PASSWORD'),
 ];
 
 $readonly_users = [];
